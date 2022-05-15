e78f7867cbe8e85c3b83f38ee829c940af0e29be
Analía Alvado <104107403+Analia1961@users.noreply.github.com>
2022-05-15 02:20:11 -0300

    corrección_IngresoDatos Al ejemplo le agregué que indique la altura de la persona

--- src/datos.ts
+++ src/datos.ts
@@ -1,12 +1,25 @@
-// Ejercicio1_Clase2 Ingreso de datos: ejemplo
+// Ingreso de datos: ejemplo
 
+let btnEnv = document.getElementById("btnEnviar");
+let formulario = document.forms[0];
+let dato = formulario.dato;
+let rotulo = document.getElementById("rotulo");
+
+btnEnv.addEventListener("click", () => {
+  // TU CÓDIGO VA AQUÍ, POR EJEMPLO:
+  console.log("La altura es", dato.value);
+  //}
+});
+
+rotulo.innerHTML = "Indique la altura de la persona: ";
+
+/*
 let btnEnv = document.getElementById("btnEnviar");
 let dato = document.forms[0];
 let rotulo = document.getElementById("rotulo");
 
 btnEnv.addEventListener("click" , () => {
 console.log("el dato ingresado es",dato.value);
-});
+});*/
 
-
-rotulo.innerHTML = "Ingrese un dato: ";
+//rotulo.innerHTML = "Ingrese un dato: ";
